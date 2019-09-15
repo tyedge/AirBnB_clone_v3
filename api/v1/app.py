82c75c17915d7d7366b5b9cd565a84755112fb21
tyedge <tyedge.inc@gmail.com>
2019-09-15 16:45:16 +0000

    Pep8'd file

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,10 +10,12 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_appcontext(code):
     "closes storage"
     storage.close()
+
 
 @app.errorhandler(404)
 def errorhandler(error):

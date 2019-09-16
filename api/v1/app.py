9743353cd76553fd0d3ee68218506f2b64794b8d
tyedge <tyedge.inc@gmail.com>
2019-09-16 04:02:24 +0000

    fixed pep8 error

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
+
 @app.teardown_appcontext
 def teardown_appcontext(code):
     """ closes storage """

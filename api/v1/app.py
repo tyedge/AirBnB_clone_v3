f3a437e2e597811d629c11ee7b48c408aefac9ec
tyedge <tyedge.inc@gmail.com>
2019-09-16 03:56:42 +0000

    changed origin to origins

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={"/*": {"origin": "0.0.0.0"}})
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown_appcontext(code):
